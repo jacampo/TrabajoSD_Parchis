e991bb0230bc9f21075e7316179af2ce9feec2ab
migonzor <migonzor@CCTL131-10.unirioja.loc>
2019-11-14 12:48:19 +0100

    Cliente cambiado

--- src/web/Cliente.java
+++ src/web/Cliente.java
@@ -1,10 +1,37 @@
 package web;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 public class Cliente {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		BufferedReader br;
+		BufferedWriter bw;
+		Socket s;
+		try {
+			s = new Socket("10.11.62.10", 8080);
+			br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+			
+			
+			System.out.println(br.readLine());
+			bw.write("soy el cliente \r\n");
+			bw.flush();
+			
+			
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			
+		}
 	}
 
 }
