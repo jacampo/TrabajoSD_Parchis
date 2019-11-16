48bed2d566f93987f7e9638bba8b98e3751d0ced
jacampo <Usuario@DESKTOP-71D07IQ>
2019-11-16 21:31:45 +0100

    fasefinal (color)

--- src/juego/FaseFinal.java
+++ src/juego/FaseFinal.java
@@ -4,16 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FaseFinal {
-	
+	private Color color;
 	private  List<Casilla> casillas;
 	
 	public FaseFinal(Color color) {
-		// TODO Auto-generated constructor stub
+		this.color=color;
 		this.casillas=new ArrayList<>(8);		
 		for(int i=0;i<7;i++) {
-			this.casillas.add(new Casilla(i+1,true,color));
+			this.casillas.add(new Casilla(i+1,true,this.color));
 		}
-		this.casillas.add(new CasillaFinal(8,true,color));
+		this.casillas.add(new CasillaFinal(8,true,this.color));
 	}
 
+	public List<Casilla> getCasillas(){
+		return this.casillas;
+	}
+	
+	public Color getColor(){
+		return this.color;
+	}
+	
+	
+	
+	
+	
 }
