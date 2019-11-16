d4177aa33c76f7a980a3da14f448c044fb153a59
migue <migue@192.168.1.131>
2019-11-16 21:56:47 +0100

    casillafinal

--- src/juego/CasillaFinal.java
+++ src/juego/CasillaFinal.java
@@ -7,5 +7,21 @@ public class CasillaFinal extends Casilla{
 		// TODO Auto-generated constructor stub
 		this.fichas=new Ficha[4];
 	}
-
+	public void colocarFicha(Ficha f) {
+		for(int i=0;i<4;i++) {
+			if(this.fichas[i]!=null) {
+				this.fichas[i] = f;
+			}
+		}
+	}
+	
+	public boolean haGanado() {
+		int i=0;
+		while(i<4) {
+			if(this.fichas[i]==null)
+				return false;
+			i++;
+		}
+		return true;
+	}
 }
