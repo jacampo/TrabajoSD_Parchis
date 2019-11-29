893a41f03bfd630cb2bc58b55a7c3788feddcc08
jacampo <Usuario@DESKTOP-71D07IQ>
2019-11-29 19:32:00 +0100

    partida

--- src/juego/Partida.java
+++ src/juego/Partida.java
@@ -1,10 +1,55 @@
 package juego;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Vector;
+
 public class Partida {
 
-	public static void main(String[] args) {
+	private Tablero tablero;
+	private Vector<Jugador> jugadores;
+	private Dado dado;
+	
+	public Partida(){
+		this.tablero = new Tablero();
+		this.jugadores = new Vector<>(4);
+		this.jugadores.add(new Jugador(Color.Amarillo, tablero));
+		this.jugadores.add(new Jugador(Color.Azul, tablero));
+		this.jugadores.add(new Jugador(Color.Rojo, tablero));
+		this.jugadores.add(new Jugador(Color.Verde, tablero));
+		this.dado = new Dado(6);
 		
-
+		
+		
 	}
+	
+	
+	public void jugar() {
+		while(!this.terminado()) {
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+	}
+	
+
+	//primerturno
+	
+	
+	public boolean terminado() {
+		
+	}
+	
+	//ganador
+	
+	//clasificacion
 
 }
