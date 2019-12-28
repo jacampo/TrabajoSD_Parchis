f3f2bdf957ea15d9bd54b5bde1f17830d1f9ee58
jacampo <Usuario@DESKTOP-71D07IQ>
2019-12-28 20:55:44 +0100

    fin

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,6 +1,6 @@
 
 
-package Cliente;
+package cliente;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Cliente {
@@ -16,11 +17,13 @@ public class Cliente {
 		BufferedReader br;
 		BufferedWriter bw;
 		Socket s;
+		Scanner sca = null;
 		try {
 			s = new Socket("localhost", 8080);
 			br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
 			
+			sca=new Scanner(System.in);
 			String lin=br.readLine();
 			while(!lin.equalsIgnoreCase("FIN")) {			
 				while(!lin.startsWith("DADO")) {
@@ -32,10 +35,20 @@ public class Cliente {
 				while(!lin.equalsIgnoreCase("ok")) {					
 					if(lin.equalsIgnoreCase("ERROR")) {
 						System.out.println("introduce una ficha valida");
-					}					
-					System.out.println("introduce el numero de la ficha que quieres cambiar");
-					Scanner sca=new Scanner(System.in);
-					int fich=sca.nextInt();
+					}			
+					String fich = "";
+					int a = 0;
+					do {
+						System.out.println("introduce el numero de la ficha que quieres cambiar");
+						try {
+							fich = sca.next();
+							a = Integer.parseInt(fich);
+						}catch (NumberFormatException e) {
+							System.out.println("No es un numero");
+							a = 0;
+						}
+					}while(a < 1 || a > 4);
+					
 					
 					bw.write("FICHA "+fich+"\r\n");
 					bw.flush();
@@ -59,7 +72,13 @@ public class Cliente {
 			e.printStackTrace();
 		}
 		finally {
-			
+			if(sca!=null) {
+				try {
+					sca.close();
+				}catch (IllegalStateException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
---
+++ src/servidor/Servidor.java
@@ -0,0 +1,55 @@
+package servidor;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import juego.Partida;
+
+public class Servidor {
+
+	public static void main(String[] args) {
+		
+		ExecutorService pool = Executors.newFixedThreadPool(4);
+		List<Future<Socket>> l = new LinkedList<Future<Socket>>();
+		try {
+			ServerSocket ss = new ServerSocket(8080);		
+			while(true) {
+				try {				
+					
+					Socket s1 = ss.accept();
+					Socket s2 = ss.accept();
+					Socket s3 = ss.accept();
+					Socket s4 = ss.accept();
+					
+					Vector<Socket> v = new Vector<>();
+					v.add(s1);
+					v.add(s2);
+					v.add(s3);
+					v.add(s4);
+					
+					
+					Partida p = new Partida(v);
+					p.jugar();
+				}
+				catch(IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+			
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			pool.shutdown();
+		}
+	}
+}
